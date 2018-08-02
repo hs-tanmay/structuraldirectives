0d30c613a6bf7b668254bd36f5edb8506d8e5fa8
Tanmay <tanmay.kadam@qed42.com>
2018-08-03 00:44:44 +0530

    Code for structural directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgSwitchDemoComponent } from './ng-switch-demo/ng-switch-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgSwitchDemoComponent
   ],
   imports: [
     BrowserModule
